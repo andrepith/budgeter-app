c3af8112df4ff8e8b16b703b11d20959bc8edc93
Andre <andrepith@yahoo.co.uk>
2018-08-02 20:35:32 +0700

    Fix

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,26 +1,45 @@
-// object destructuring
+//
+// Object destructuring
+//
 
-// const book = {
-//     title: 'Ego is the enemy',
-//     author: 'Ryan Holiday',
-//     publisher: {
-//         // name: 'Penguin',
-//     }
+// const person = {
+//   name: 'Andrew',
+//   age: 27,
+//   location: {
+//     city: 'Philadelphia',
+//     temp: 88
+//   }
 // };
 
-// const { name: publisherName = 'Self Published' } = book.publisher;
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`);
 
-// console.log(publisherName);
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}.`);
+// }
 
-// array destructuring
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     // name: 'Penguin'
+//   }
+// };
 
-const item = [
-    'Coffee (hot)',
-    '$2.00',
-    '$2.50',
-    '$2.75'
-];
+// const { name: publisherName = 'Self-Published' } = book.publisher;
 
-const [ itemName, , medium] = item;
+// console.log(publisherName); // Penguin, Self-Published
 
-console.log(`A medium ${itemName} costs ${medium}`);
+//
+// Array destructuring
+//
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,66 +1,67 @@
 import { createStore } from 'redux';
 
+// Action generators - functions that return action objects
+
 const incrementCount = ({ incrementBy = 1 } = {}) => ({
-    type: 'INCREMENT',
-    incrementBy
+  type: 'INCREMENT',
+  incrementBy
 });
 
 const decrementCount = ({ decrementBy = 1 } = {}) => ({
-    type: 'DECREMENT',
-    decrementBy
+  type: 'DECREMENT',
+  decrementBy
 });
 
-const setCount = ({ setBy }) => ({
-    type: 'SET',
-    setBy
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
 });
 
 const resetCount = () => ({
-    type: 'RESET'
+  type: 'RESET'
 });
 
 // Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or actiton
 
-const countReducer = ((state = { count: 0 }, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return {
-                count: state.count + action.incrementBy
-            };
-            break;
-        case 'DECREMENT':
-            return {
-                count: state.count - action.decrementBy
-            };
-            break;
-        case 'SET':
-            return {
-                count: action.setBy
-            };
-            break;
-        case 'RESET':
-            return {
-                count: 0
-            };
-            break;
-        default:
-            return state;
-            break;
-    }
-});
+const countReducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.incrementBy
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - action.decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+};
 
 const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
-    console.log(store.getState());
+  console.log(store.getState());
 });
 
-store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(incrementCount({ incrementBy: 5 }))
+
 store.dispatch(incrementCount());
 
 store.dispatch(resetCount());
 
 store.dispatch(decrementCount());
-store.dispatch(decrementCount( { decrementBy: 10 }));
 
-store.dispatch(setCount( {setBy: 150} ));
+store.dispatch(decrementCount({ decrementBy: 10 }));
+
+store.dispatch(setCount({ count: -100 }));
